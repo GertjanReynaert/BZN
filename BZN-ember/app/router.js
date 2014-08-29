3210582e5bcc1bdcc71e89e99b846b8afacc9d5e
Gertjan Reynaert <gertjan@typework.com>
2014-08-29 16:15:05 +0200

    extended router, edited proms

--- BZN-ember/app/router.js
+++ BZN-ember/app/router.js
@@ -12,7 +12,18 @@ Router.map(function() {
 
   this.route('control', { path: '/controle' });
 
-  this.route('sales', { path: '/verkoop' });
+  this.resource('sales', { path: '/verkoop' }, function() {
+    this.route('delivery', { path: '/levering' });
+    this.route('stock');
+    this.route('history', { path: '/geschiedenis' });
+    this.route('large', { path: '/:name' });
+    this.route('till', { path: '/kas' });
+  });
+
+  this.resource('sale', { path: '/nieuwe-verkoop' }, function() {
+    this.route('new');
+    this.route('payment');
+  });
 
   this.resource('proms', function() {
     this.route('new');
