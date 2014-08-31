9fabde47655967c05ca60ee80c75bb774ba5aafc
Gertjan Reynaert <gertjan@typework.com>
2014-08-31 10:07:01 +0200

    First build

--- BZN-ember/Brocfile.js
+++
@@ -1,9 +0,0 @@
-/* global require, module */
-
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
-var app = new EmberApp();
-
-app.import("vendor/ember-localstorage-adapter/localstorage_adapter.js");
-
-module.exports = app.toTree();
--- BZN-ember/tests/helpers/resolver.js
+++
@@ -1,9 +0,0 @@
-import Resolver from 'ember/resolver';
-
-var resolver = Resolver.create();
-
-resolver.namespace = {
-  modulePrefix: 'bzn-ember'
-};
-
-export default resolver;
