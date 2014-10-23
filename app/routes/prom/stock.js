f51b662bcf800fed4ee75d77a5429d75f77fa401
Gertjan Reynaert <gertjan@typework.com>
2014-10-23 09:09:13 +0200

    trying to fix get stock

--- app/routes/prom/stock.js
+++ app/routes/prom/stock.js
@@ -2,9 +2,6 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
   model: function() {
-    var prom = this.modelFor('prom');
-    return this.store.find(prom).then(function(prom) {
-      return prom.get('stock');
-    });
+    return this.modelFor('prom').get('stock');
   }
 });
